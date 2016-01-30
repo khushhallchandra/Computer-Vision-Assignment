f9777c5417d0355cb8008034f342609cd41439a1
khushhallchandra <kcm00000@gmail.com>
2016-01-30 23:39:42 +0530

    Completed question 6

---
+++ assignment_1/6/code/myMainScript.m
@@ -0,0 +1,44 @@
+%% MyMainScript
+
+tic;
+%This correspond to p1 - 3xn
+img1 = importdata('../data/Features2D_dataset1.mat');
+p1 = img1(1:2, :)';
+
+%This correspond to p - 4xn
+img2 = importdata('../data/Features3D_dataset1.mat');
+p = img2(1:3, :)';
+
+n = size(img1, 2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Adding noise;this may be commented
+%maxc = 0.05 * max(max(abs(img2)));
+%p1 = p1 + (maxc .* randn([n 2])); 
+%p  = p  + (maxc .* randn([n 3]));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+a = [p, ones(n, 1), zeros(n, 4)];
+q = -1 * p .* p1(:, 1) ;
+a = [a q (-1 * p1(:, 1))];
+
+b = [zeros(n, 4), p, ones(n, 1)];
+r = -1 * p .* p1(:, 2) ;
+b = [b r (-1 * p1(:, 2))];
+
+A = [ a; b];
+
+[U, D, V] = svd(A);
+
+% Taking the last row of matrix V so
+%that we have row of V corresponding
+% to min eigen value
+m = V(:, 12)';
+M = reshape(m, 4, 3)';
+
+p1val = M * img2;
+p1val = p1val ./ p1val(3,:);
+
+norm(p1val - img1)/sqrt(norm(p1val) * norm(img1))
+
+toc;
