cd2731a7521a13c124bfce85959e73e921c2945c
khushhallchandra <kcm00000@gmail.com>
2016-01-31 04:44:11 +0530

    Program to wrap the image and fill the unmatched point to black

---
+++ assignment_1/7/code/myRevWrap.m
@@ -0,0 +1,21 @@
+function out = myRevWrap(img,H)
+
+	[ x ,y ] = size(img);
+	x_vec = repmat(1:x, y,1);
+	y_vec = repmat(1:y, 1, x);
+
+	imgT = [ x_vec(:)' ; y_vec ; ones(1,x*y) ];
+	im = inv(H)*imgT;
+	im = round(im ./ (ones(3,1)*im(3,:)));
+	im = im(1:2, :);
+
+	v = zeros(x*y,1);
+	for i = 1 : x*y
+	    if ((im(1,i)<1 || im(1,i)>x) || (im(2,i)<1 || im(2,i)>y))
+		v(i) =0; 
+	    else
+		v(i)=img(im(1,i),im(2,i));
+	    end
+	end
+	out = uint8(reshape(v,y,x)');
+end
