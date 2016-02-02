ce4ba9f4b589269047b6bb5841a1d97fac6aa7c3
khushhallchandra <kcm00000@gmail.com>
2016-02-02 22:32:25 +0530

    Completed Q1 to Q6

--- assignment_1/6/code/myMainScript1.m
+++ assignment_1/6/code/myMainScript1.m
@@ -1,12 +1,10 @@
-%% MyMainScript
-
-tic;
+%% MyMainScript1
 %This correspond to p1 - 3xn
-img1 = importdata('../data/Features2D_dataset2.mat');
+img1 = importdata('../data/Features2D_dataset1.mat');
 p1 = img1(1:2, :)';
 
 %This correspond to p - 4xn
-img2 = importdata('../data/Features3D_dataset2.mat');
+img2 = importdata('../data/Features3D_dataset1.mat');
 p = img2(1:3, :)';
 
 n = size(img1, 2);
@@ -19,11 +17,11 @@ n = size(img1, 2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 a = [p, ones(n, 1), zeros(n, 4)];
-q = -1 * p .* p1(:, 1) ;
+q = -1 * p .* (p1(:, 1)*ones(1,3)) ;
 a = [a q (-1 * p1(:, 1))];
 
 b = [zeros(n, 4), p, ones(n, 1)];
-r = -1 * p .* p1(:, 2) ;
+r = -1 * p .* ( p1(:, 2)*ones(1,3)) ;
 b = [b r (-1 * p1(:, 2))];
 
 A = [ a; b];
@@ -31,14 +29,17 @@ A = [ a; b];
 [U, D, V] = svd(A);
 
 % Taking the last row of matrix V so
-%that we have row of V corresponding
+% that we have row of V corresponding
 % to min eigen value
 m = V(:, 12)';
 M = reshape(m, 4, 3)';
 
 p1val = M * img2;
-p1val = p1val ./ p1val(3,:);
+p1val = p1val ./ (ones(3,1)*p1val(3,:));
 
-norm(p1val - img1)/sqrt(norm(p1val) * norm(img1)) *100
-
-toc;
+%% Value of M
+M
+%% Error between original and calculated image
+error = norm(p1val - img1)/sqrt(norm(p1val) * norm(img1)) * 100;
+% value of e (in percentage)
+error
---
+++ assignment_1/6/code/myMainScript2_noisy.m
@@ -0,0 +1,45 @@
+%% MyMainScript2_noisy
+%This correspond to p1 - 3xn
+img1 = importdata('../data/Features2D_dataset2.mat');
+p1 = img1(1:2, :)';
+
+%This correspond to p - 4xn
+img2 = importdata('../data/Features3D_dataset2.mat');
+p = img2(1:3, :)';
+
+n = size(img1, 2);
+
+%% Adding noise
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+maxc = 0.05 * max(max(abs(img2)));
+p1 = p1 + (maxc .* randn([n 2])); 
+p  = p  + (maxc .* randn([n 3]));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+a = [p, ones(n, 1), zeros(n, 4)];
+q = -1 * p .* (p1(:, 1)*ones(1,3)) ;
+a = [a q (-1 * p1(:, 1))];
+
+b = [zeros(n, 4), p, ones(n, 1)];
+r = -1 * p .* ( p1(:, 2)*ones(1,3)) ;
+b = [b r (-1 * p1(:, 2))];
+
+A = [ a; b];
+
+[U, D, V] = svd(A);
+
+% Taking the last row of matrix V so
+% that we have row of V corresponding
+% to min eigen value
+m = V(:, 12)';
+M = reshape(m, 4, 3)';
+
+p1val = M * img2;
+p1val = p1val ./ (ones(3,1)*p1val(3,:));
+
+%% Value of M
+M
+%% Error between original and calculated image
+error = norm(p1val - img1)/sqrt(norm(p1val) * norm(img1)) * 100;
+% value of error (in percentage)
+error
