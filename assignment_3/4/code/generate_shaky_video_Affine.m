7c5e6849edbdcb33622b4205cd34501bd6ee71fe
khushhallchandra <kcm00000@gmail.com>
2016-03-05 06:01:51 +0530

    Started and arranged the folder

---
+++ assignment_3/4/code/generate_shaky_video_Affine.m
@@ -0,0 +1,27 @@
+clear;
+
+a = mmread('..\data\sampleVideo\cars.avi');
+framerate = a.rate;
+vid = zeros(a.height,a.width,a.nrFramesTotal);
+
+for i=1:a.nrFramesTotal 
+    b = rgb2gray(a.frames(i).cdata); 
+    [H,W] = size(b);
+    if i > 1, tx = round(rand(1)*3); else tx = 0; end;
+    if i > 1, ty = round(rand(1)*3); else ty = 0; end;
+    if i > 1,
+        A = [1 0; 0 1] + rand(2,2)*0.1;
+    else
+        A = [1 0; 0 1];
+    end;
+           
+    c = my_affine_warp(b,A);    
+    d = c; d(:,:) = 0;
+    d(ty+1:H,tx+1:W) = c(1:H-ty,1:W-tx);
+        
+    vid(:,:,i) = d;
+end
+  
+filename = '..\data\shakyAffine\shaky_cars.avi';
+writevideo(filename,vid/max(vid(:)),framerate);
+    
