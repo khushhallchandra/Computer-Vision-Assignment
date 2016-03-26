065df975b7c3e4f652aec161047ec26723f4c219
khushhallchandra <kcm00000@gmail.com>
2016-03-26 23:35:35 +0530

    Added

--- assignment_4/1/code/myMainScript.m
+++ assignment_4/1/code/myMainScript.m
@@ -1,16 +1,16 @@
 %% Load Dataset
-[X_train, Y_train, X_test, Y_test] = loadData('partC');
+[X_train, Y_train, X_test, Y_test] = loadData('partB');
 
 %%Plot the data
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-figure
-plus = X_train(Y_train == 1,:);
-minus = X_train(Y_train == -1,:);
-scatter(plus(:,1),plus(:,2),'b')
-hold on
-scatter(minus(:,1),minus(:,2),'r')
-legend('+1','-1')
-title('Plot of training data');
+%figure
+%plus = X_train(Y_train == 1,:);
+%minus = X_train(Y_train == -1,:);
+%scatter(plus(:,1),plus(:,2),'b')
+%hold on
+%scatter(minus(:,1),minus(:,2),'r')
+%legend('+1','-1')
+%title('Plot of training data');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 epoch = 40;
@@ -19,7 +19,7 @@ i = zeros(epoch,1);
 p = zeros(epoch,1);
 theta = zeros(epoch,1);
 alpha = zeros(epoch,1);
-weight = ones(size(Y_train))/size(X_train,1);
+weight = ones(size(Y_train))/size(Y_train,1);
 
 for t = 1:epoch
     %% Find best classifier with current weights
@@ -49,22 +49,26 @@ for t = 1:epoch
     label = (Y_train ~= sign(p(t)*(X_train(:,i(t)) - theta(t))));    
     weight(label) = weight(label)*exp(alpha(t));
     weight(~label) = weight(~label)*exp(-alpha(t));    
-    weight = weight/sum(weight);    
-end
-% Calculating the errors
+    weight = weight/sum(weight);
 
-%Training data
-Y_train_predicted = zeros(size(Y_train));
-for k = 1:epoch
-    Y_train_predicted += alpha(k)*sign(p(k)*(X_train(:,i(k)) - theta(k)));
-end
-Y_train_predicted = sign(Y_train_predicted);
-train_error = sum(Y_train~=Y_train_predicted)*100/length(Y_train)
+	%% Calculating the errors
+	
+	% estimate and print the training error 
+	% of the strong classifier created thus far  
+	t 
+	Y_train_predicted = zeros(size(Y_train));
+	for k = 1:t
+		Y_train_predicted += alpha(k)*sign(p(k)*(X_train(:,i(k)) - theta(k)));
+	end
+	Y_train_predicted = sign(Y_train_predicted);
+	train_error = sum(Y_train~=Y_train_predicted)*100/length(Y_train) 
 
-% Test data
-Y_strong_test = zeros(size(Y_test));
-for k = 1:epoch
-    Y_strong_test += alpha(k)*sign(p(k)*(X_test(:,i(k)) - theta(k)));
+	% estimate and print the test error of 
+	% the strong classifier created thus far 
+	Y_strong_test = zeros(size(Y_test));
+	for k = 1:t
+		Y_strong_test += alpha(k)*sign(p(k)*(X_test(:,i(k)) - theta(k)));
+	end
+	Y_strong_test = sign(Y_strong_test);
+	test_error = sum(Y_test~=Y_strong_test)*100/length(Y_test)
 end
-Y_strong_test = sign(Y_strong_test);
-test_error = sum(Y_test~=Y_strong_test)*100/length(Y_test)
